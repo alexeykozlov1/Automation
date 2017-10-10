8162801bbd0268fbaadfad34b173ec4449f187c6
Alexey Kozlov <alexeymspm@gmail.com>
2017-10-10 15:22:47 -0400

    Connection to Git from Intellij deleted

--- sandbox/src/main/java/ru/stqa/tft/sandbox/Hello.java
+++ sandbox/src/main/java/ru/stqa/tft/sandbox/Hello.java
@@ -1,7 +1,8 @@
 package ru.stqa.tft.sandbox;
 
 public class Hello {
-    public static void main(String[] args){
-    System.out.println("ru.stqa.tft.sandbox.Hello world");
+    public static void main(String[] args) {
+
+        System.out.println("ru.stqa.tft.sandbox.Hello world");
     }
 }
