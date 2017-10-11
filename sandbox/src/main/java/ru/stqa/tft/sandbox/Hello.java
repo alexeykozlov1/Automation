740ac079c120154f5f8cc5b835377d247d67c5ef
Alexey Kozlov <alexeymspm@gmail.com>
2017-10-11 13:22:52 -0400

    added method

--- sandbox/src/main/java/ru/stqa/tft/sandbox/Hello.java
+++ sandbox/src/main/java/ru/stqa/tft/sandbox/Hello.java
@@ -1,8 +1,14 @@
 package ru.stqa.tft.sandbox;
 
 public class Hello {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        System.out.println("ru.stqa.tft.sandbox.Hello world");
-    }
+    hello("User");
+  }
+
+  public static void hello(String somebody) {
+
+    System.out.println("Hello " + somebody + "!");
+  }
+
 }
